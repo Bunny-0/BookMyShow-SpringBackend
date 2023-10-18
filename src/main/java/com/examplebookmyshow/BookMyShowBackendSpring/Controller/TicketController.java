f0e33829e8cc96c7e7f71a259ab12c0b008cf4c8
shubham gaurav <shubhamgaurav2@gmail.com>
2023-10-18 20:39:12 +0530

    "Completing added ticket Controllers"

--- src/main/java/com/examplebookmyshow/BookMyShowBackendSpring/Controller/TicketController.java
+++ src/main/java/com/examplebookmyshow/BookMyShowBackendSpring/Controller/TicketController.java
@@ -1,4 +1,24 @@
 package com.examplebookmyshow.BookMyShowBackendSpring.Controller;
 
+import com.examplebookmyshow.BookMyShowBackendSpring.Dto.EntryDto.TicketEntryDto;
+import com.examplebookmyshow.BookMyShowBackendSpring.Dto.ResponseDto.TicketResponseDto;
+import com.examplebookmyshow.BookMyShowBackendSpring.Service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("Ticket")
 public class TicketController {
+
+    @Autowired
+    TicketService ticketService;
+
+    @PostMapping("/createTicket")
+    public TicketResponseDto createTicket(@RequestBody TicketEntryDto ticketEntryDto){
+        return ticketService.createTicket(ticketEntryDto);
+
+    }
 }
